521bd81ec03eb0c3fc47262e0f182ea1c3092c34
seungrii <choevictory@gmail.com>
2023-09-07 13:16:24 +0900

    singleton

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,25 +17,24 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
 
         //ThreadA: A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         //ThreadB: B사용자 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
+//        int price = statefulService1.getPrice();
 
         //ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
     static class TestConfig {
 
         @Bean
         public StatefulService statefulService() {
             return new StatefulService();
-            //
         }
     }
 }
